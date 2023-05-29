09143882d112a3a390ae8da71a4ffaf5c0b2ec81
Kévin Bernard <kde.bernard.91@gmail.com>
2023-05-29 22:58:29 +0200

    :sparkles: Connect the Prepare Session Page and Session Page

--- client/src/components/PrepareSessionLayout.js
+++ client/src/components/PrepareSessionLayout.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ListExercise from "./ListExercise";
+import { Navigate } from "react-router-dom";
 
 const listExo = require("../listExercise.json")
 
 class PrepareSessionLayout extends React.Component {
     constructor(props) {
         super(props)
+        sessionStorage.removeItem("listExoChoice")
         this.handleChoiceExercises = this.handleChoiceExercises.bind(this)
         this.state = {
-            listChoice: []
+            listChoice: [],
+            submitted: false,
         }
     }
 
@@ -16,8 +19,29 @@ class PrepareSessionLayout extends React.Component {
         this.setState({listChoice: listSelectedExercise})
     }
 
+    handleSubmit = () => {
+        const listChoice = this.state.listChoice
+        console.log("Submit session : Size of listChoice = " + listChoice.length)
+        if (listChoice.length != 0) {
+            const strListChoise = this.listExoToString(listChoice)
+            sessionStorage.setItem("listExoChoice", strListChoise)
+            this.setState({submitted: true});
+        }
+    }
+
+    listExoToString(listExo) {
+        let resultString = ""
+        listExo.forEach(element => {
+            console.log(element.name)
+            resultString = resultString + element.name + "-" + element.id_name + "/" 
+        });
+        console.log(resultString)
+        return resultString;
+    }
+
     render() {
         const listChoice = this.state.listChoice
+        const submitted = this.state.submitted
         return <>
             <div>
                 <h2>Liste d&apos;exercices</h2>
@@ -26,7 +50,9 @@ class PrepareSessionLayout extends React.Component {
             <div>
                 <h2>Choix d&apos;exercices pour la session</h2>
                 <ListExercise listExo={listChoice} goal="Validation" exoChoice={listChoice}/>
+                <button type="submit" value="Valider" onClick={this.handleSubmit}>Valider</button>
             </div>
+            {submitted && <Navigate to="/session" />}
         </>
     }
 }

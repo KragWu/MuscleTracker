460708c33c04c94c5c6a8bb624ea7d972b0b8f39
Kévin Bernard <kde.bernard.91@gmail.com>
2023-08-04 23:10:18 +0200

    :sparkles: Add User Service for App

---
+++ client/src/services/UserService.js
@@ -0,0 +1,66 @@
+async function register(login, password) {
+    const body = {
+        "login": login,
+        "password": password
+    }
+    console.log("call User API register")
+    const response = await fetch('http://localhost:8080/register', { method: 'POST', mode: "cors", body: JSON.stringify(body), headers: {'Content-Type': 'application/json'}});
+    const data = await response.text()
+    if (response.status != 201) {
+        throw new Error("failed register")
+    } else {
+        return data
+    }
+}
+
+async function login(login, password) {
+    const body = {
+        "login": login,
+        "password": password
+    }
+    console.log("call User API login")
+    // Can add userid in headers
+    const response = await fetch('http://localhost:8080/login', { method: 'POST', mode: "cors", body: JSON.stringify(body), headers: {'Content-Type': 'application/json'}});
+    const data = await response.text()
+    if (response.status != 200) {
+        throw new Error("failed login")
+    } else {
+        const obj = JSON.parse(data)
+        console.log(obj.loginDateTime)
+        return obj
+    }
+}
+
+async function authorize(idSession, token) {
+    const body = {
+        "id": idSession,
+        "token": token
+    }
+    console.log("call User API authorize")
+    // Can add userid in headers
+    const response = await fetch('http://localhost:8080/authorize', { method: 'POST', mode: "cors", body: JSON.stringify(body), headers: {'Content-Type': 'application/json'}});
+    const data = await response.text()
+    if (response.status != 200) {
+        throw new Error("failed authorize")
+    } else {
+        return data
+    }
+}
+
+async function logout(idSession, token) {
+    const body = {
+        "id": idSession,
+        "token": token
+    }
+    console.log("call User API logout")
+    // Can add userid in headers
+    const response = await fetch('http://localhost:8080/logout', { method: 'POST', mode: "cors", body: JSON.stringify(body), headers: {'Content-Type': 'application/json'}});
+    const data = await response.text()
+    if (response.status != 200) {
+        throw new Error("failed logout")
+    } else {
+        return data
+    }
+}
+
+export {register, login, authorize, logout}
